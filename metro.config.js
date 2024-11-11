4753ac61884a89192689097177ad3fd7ced34f1b
Marvin <marvinvilaysack@gmail.com>
2024-11-11 17:25:03 -0600

    working functionalities

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,15 @@
+// metro.config.js
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Add the polyfills
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  stream: require.resolve('stream-browserify'),
+  'stream-browserify': require.resolve('stream-browserify'),
+};
+
+// Export with NativeWind configuration
 module.exports = withNativeWind(config, { input: "./global.css" });
